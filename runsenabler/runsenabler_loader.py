1c3520e911044059a0f1087f1a6eeafc98d45879
labuser <labuser@lab>
2019-06-28 10:37:52 +0100

    implemented filesystem version of the runsnenabler which copies runs between temp directory and actual logdir without symlinks

--- runsenabler/runsenabler_loader.py
+++ runsenabler/runsenabler_loader.py
@@ -1,9 +1,12 @@
 from tensorboard.plugins import base_plugin
 from .runsenabler_plugin import RunsEnablerPlugin
 
+from .runsenabler_controller import EventMultiplexerRunsController, FilesystemRunsController
+
 class RunsEnablerLoader(base_plugin.TBLoader):
-    def __init__(self):
+    def __init__(self, logdir):
         self._plugin_class = RunsEnablerPlugin
+        self.actual_logdir = logdir
     
     def define_flags(self, parser):
         group = parser.add_argument_group('runsenabler plugin')
@@ -13,6 +16,11 @@ class RunsEnablerLoader(base_plugin.TBLoader):
         group.add_argument('--enable_profiling', metavar='ENABLENPROF', type=bool, default=False, help='''\
             Determines whether runsenabler will write stats to disk.\
             ''')
+        group.add_argument("--use_filesystem_controller", type=bool, default=True, store_true=True)
 
     def load(self, context):
-        return self._plugin_class(context)
+        # Determine which controller to use - either use the multiplexer directly or manipulate runs at the the filesystem level
+        controller = EventMultiplexerRunsController(context._multiplexer, self.actual_logdir)
+        if context.flags.use_filesystem_controller:
+            controller = FilesystemRunsController(self.actual_logdir, controller)
+        return self._plugin_class(context, controller)
